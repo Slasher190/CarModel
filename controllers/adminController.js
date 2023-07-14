ca60a3db42739dd0fb7c354f80df5903bae2d480
Sudhi Ranjan Gupta <57698770+Slasher190@users.noreply.github.com>
2023-07-14 17:07:55 +0530

    Update adminController.js

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -1,15 +1,18 @@
 import ErrorHandler from "../middleware/error";
 import Admin from "../models/adminModel";
-import { sendCookie } from "../utils/features";
 
 export const registerAdmin = async (req, res, next) => {
   try {
     const { admin_id, password } = req.body;
-    const admin = Admin.find({admin_id});
-    if (admin_id){
-      return next(new ErrorHandler("Admin already exist"))
+    const admin = Admin.find({ admin_id });
+    if (admin) {
+      return next(new ErrorHandler("Admin already exist"));
     }
     await Admin.createAdmin({ admin_id, password, created_at: new Date.now() });
-    sendCookie()
-  } catch (error) {}
+    res.status(200).json({
+      success: true,
+    });
+  } catch (error) {
+    next(error);
+  }
 };
